94efff89bc42e082ccc214799f7c817ee5f83037
jhero <jherome.samson90@gmail.com>
2021-03-02 20:53:38 +0100

    Trovare il massimo numero di event per actor Trovare il minimo numero di event per actor Trovare il massimo numero di event per repo Trovare il minimo numero di event per repo

--- src/main/scala/ManagerDATAFRAME.scala
+++ src/main/scala/ManagerDATAFRAME.scala
@@ -5,74 +5,100 @@ import org.apache.spark.sql.functions.{col, explode, hour, minute, second}
 class ManagerDATAFRAME(val dataFrame : sql.DataFrame, val SQLContext: SQLContext) {
 
   //Trovare i singoli «actor»;
-  def SingoliActor(): sql.DataFrame =
-    dataFrame.select("actor").distinct()
+  def SingoliActor(): Unit =
+    dataFrame.select("actor").distinct().show()
 
 //Trovare i singoli «actor»;
-  def SingoliAuthorNelCommit(): sql.DataFrame = {
+  def SingoliAuthorNelCommit(): Unit = {
 
     //dataFrame.show()
     val payloadDF = dataFrame.select("payload.*").distinct()
 
     val commitsDF = payloadDF.select(explode(col("commits"))).select("col.*")
 
-    commitsDF.select("author")
+    commitsDF.select("author").show()
   }
 
   //Trovare i singoli «actor»;
-  def SingloliRepo():  sql.DataFrame = {
-    dataFrame.select("repo").distinct()
+  def SingloliRepo():  Unit = {
+    dataFrame.select("repo").distinct().show()
   }
 
   //Trovare i vari tipi di evento «type»;
-  def SingoliTipiType(): sql.DataFrame = {
-    dataFrame.select("`type`").distinct()
+  def SingoliTipiType(): Unit = {
+    dataFrame.select("`type`").distinct().show()
   }
 
   //Contare il numero di «actor»;
-  def NumeroActor(): Long = {
+  def NumeroActor(): Unit = {
     dataFrame.select("actor").distinct().count()
   }
 
   //Contare il numero di «repo»;
-  def NumeroRepo(): Long =  {
-    dataFrame.select("repo").distinct().count()
+  def NumeroRepo(): Unit =  {
+    val n = dataFrame.select("repo").distinct().count()
+    println(n)
   }
 
   //Contare il numero di «event» (con «event» intendo l’oggeto principale del json parsato) per ogni «actor»;
-  def NumeroEvent():  sql.DataFrame =  {
-    dataFrame.groupBy("actor").count()
+  def NumeroEvent():  Unit =  {
+    dataFrame.groupBy("actor").count().show()
   }
 
   //Contare il numero di «event», divisi per «type» e «actor»;
-  def NumeroEventPerActor():  sql.DataFrame = {
-    dataFrame.groupBy("`type`", "actor").count()
+  def NumeroEventPerActor():  Unit = {
+    dataFrame.groupBy("`type`", "actor").count().show()
   }
 
   // Contare il numero di «event», divisi per «type», «actor», «repo»;
-  def NumeroEventPerTypeActorRepo() : sql.DataFrame = {
-    dataFrame.groupBy("`type`", "actor", "repo").count()
+  def NumeroEventPerTypeActorRepo() : Unit= {
+    dataFrame.groupBy("`type`", "actor", "repo").count().show()
   }
 
   //Contare il numero di «event», divisi per «type», «actor», «repo» e ora
-  def NumeroEventPerTypeActorRepoSecondo() : sql.DataFrame = {
+  def NumeroEventPerTypeActorRepoSecondo() : Unit = {
     dataFrame.withColumn("seconds", second(col("created_at")))
     .groupBy("`type`","actor", "repo", "seconds").count().show()
-    dataFrame
+    dataFrame.show()
   }
 
   // Trovare il massimo numero di event per secondo
-  def NumeroMassimoEventPerSecondo() :  sql.DataFrame = {
+  def NumeroMassimoEventPerSecondo() :  Unit = {
     dataFrame.withColumn("seconds", second(col("created_at")))
-      .groupBy("actor").max()
+      .groupBy().max().show()
   }
   // Trovare il minimo numero di event per secondo
-  def NumeroMininoEventPerSecondo() :  sql.DataFrame = {
+  def NumeroMininoEventPerSecondo() :  Unit = {
     dataFrame.withColumn("seconds", second(col("created_at")))
-      .groupBy("actor").min()
+      .groupBy().min().show()
   }
   // Trovare il massimo numero di event per Actor
-  def NumeroMassimoEventPerActor
+  def NumeroMassimoEventPerActor(): Unit = {
+    val df = dataFrame.withColumn("actor", col("actor") )
+      .groupBy("actor").count()
+    df.select("count").groupBy().max().show()
+  }
+
+  // Trovare il Minimo numero di event per Actor
+  def NumeroMinimoEventPerActor(): Unit = {
+    val df = dataFrame.withColumn("actor", col("actor") )
+      .groupBy("actor").count()
+    df.select("count").groupBy().min().show()
+  }
+
+  // Trovare il massimo numero di event per Repo
+  def NumeroMassimoEventPerRepo(): Unit = {
+    val df = dataFrame.withColumn("repo", col("repo") )
+      .groupBy("repo").count()
+    df.select("count").groupBy().max().show()
+  }
+
+  // Trovare il Minimo numero di event per Repo
+  def NumeroMinimoEventPerRepo(): Unit = {
+    val df = dataFrame.withColumn("repo", col("repo") )
+      .groupBy("repo").count()
+    df.select("count").groupBy().min().show()
+  }
 
 
 
